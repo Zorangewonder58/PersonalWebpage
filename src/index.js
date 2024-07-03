8accd561663aa218caffb43c657e994da2c29c81
JadenWilliams <jawilliams904@outlook.com>
2024-07-03 16:56:58 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import Projects from './pages/Projects';
 import Page from './pages/components/Navbar/Navbar';
 import reportWebVitals from './reportWebVitals';
 
+
+//Test for push
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
